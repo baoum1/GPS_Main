f8e866ac6a38c0de559b015e0a9dcb7a83d0abbd
baoum1 <baoum1@students.bfh.ch>
2018-01-18 18:39:12 +0100

    Deleted main.h (macht mä nid gmäss Stauder)

--- src/main.h
+++ src/main.h
@@ -10,30 +10,7 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-#include <stm32f4xx.h>		// Processor STM32F407IG
-#include <carme.h>			// CARME Module
-#include <stdio.h>
-#include <lcd.h>
-#include "stdlib.h"					/* Standard Library	*/
-#include "string.h"					/* String Library */
 
-// delay
-#include "delay.h"
-// accelero
-#include "accelero.h"
-// gyroscope
-#include "gyro.h"
-// i2c
-#include "i2c_init.h"
-// uart
-#include "uart.h"
-// nmea
-#include "nmea.h"
-
-// === Defines =============================================
-
-#define gps_timeout	100 			// with a Loopdelay of 10ms equals 1s
-#define a_gyr_timeout	10 			// with a Loopdelay of 10ms equals 100ms
 
 
 #endif /* MAIN_H_ */
